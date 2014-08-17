3ea68f197a1f8932d6dc9e160c333a534989915b
alfred sang <i5ting@126.com>
2014-08-17 23:07:28 +0800

    add cp.sh file

--- ios/platforms/ios/www/js/app.js
+++ ios/platforms/ios/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-var app = angular.module('starter', ['ionic', 'starter.controllers'])
+var app = angular.module('starter', ['ionic', 'starter.controllers','clock.no320.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
