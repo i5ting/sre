4f2099816f55ed998bc8c101145265220be72686
i5ting <i5ting@126.com>
2016-11-17 17:58:45 +0800

    version publish commmit, please ignore

--- index.js
+++ index.js
@@ -1,50 +1,48 @@
 #!/usr/bin/env node
 var fs = require('fs')
-var userHome = require('user-home');
-var argv = process.argv;
-argv.shift();
+var userHome = require('user-home')
+var argv = process.argv
+argv.shift()
+var lines = []
 
-for(var i in argv) {
+for (var i in argv) {
   var input = argv[i]
-  if (input == '-d' || input == '--delete') {
-    console.log("remove ~/.sre alias")
-    fs.writeFileSync(userHome + '/.sre', "");
+  if (input === '-d' || input === '--delete') {
+    console.log('remove ~/.sre alias')
+    fs.writeFileSync(userHome + '/.sre', '')
     process.exit()
   }
 }
 
-for(var i in argv) {
+for (var i in argv) {
   var input = argv[i]
-  if (input == '-l' || input == '--list') {
+  if (input === '-l' || input === '--list') {
     console.log(fs.readFileSync(userHome + '/.sre').toString())
     process.exit()
   }
 }
 
-for(var i in argv) {
+for (var i in argv) {
   var input = argv[i]
-  if (input == '-h' || input == '--help') {
-    console.log("Usages: sre = scripts run easy")
-    console.log("\t sre -h or --help")
-    console.log("\t sre -d or --delete")
-    console.log("\t sre -l or --list")
+  if (input === '-h' || input === '--help') {
+    console.log('Usages: sre = scripts run easy')
+    console.log('\t sre -h or --help')
+    console.log('\t sre -d or --delete')
+    console.log('\t sre -l or --list')
     process.exit()
   }
 }
 
-var file_path = __dirname;
-var current_path = process.cwd();
+var currentPath = process.cwd()
 
-var conf = require(current_path + '/package.json')
+var conf = require(currentPath + '/package.json')
 
-console.log("available script:")
+console.log('available script:')
 
-
-var lines = []
-for(var _name in conf.scripts){
-  console.log("\t" + _name)
-  var cmd = "alias " + _name + "='npm run " + _name + "'"
+for (var _name in conf.scripts){
+  console.log('\t' + _name)
+  var cmd = 'alias ' + _name + '=\'npm run ' + _name + '\'';
   lines.push(cmd)
 }
 
-fs.writeFileSync(userHome + '/.sre', lines.join('\n'));
+fs.writeFileSync(userHome + '/.sre', lines.join('\n'))
